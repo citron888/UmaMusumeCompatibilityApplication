975740c04d7cb2da1f992204878603756a13c32f
nijuenn <karakasa.boroboro.1eyed@gmail.com>
2021-05-08 19:55:09 +0900

    ウマ娘のデータテーブル追加

---
+++ umamusu.json.js
@@ -0,0 +1,194 @@
+window.umamusu = [
+    {
+        "id": 1,
+        "name": "スペシャルウィーク",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"F","mile":"C","middle":"A","long":"A"}
+    },
+    {
+        "id": 2,
+        "name": "サイレンススズカ",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"D","mile":"A","middle":"A","long":"E"}
+    },
+    {
+        "id": 3,
+        "name": "メジロマックイーン",
+        "baba_aptitude": {"turf":"A","dirt":"E"},
+        "distance_aptitude":{"short":"G","mile":"F","middle":"A","long":"A"}
+    },
+    {
+        "id": 4,
+        "name": "トウカイテイオー",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"F","mile":"E","middle":"A","long":"B"}
+    },
+    {
+        "id": 5,
+        "name": "タイキシャトル",
+        "baba_aptitude": {"turf":"A","dirt":"B"},
+        "distance_aptitude":{"short":"A","mile":"A","middle":"E","long":"G"}
+    },
+    {
+        "id": 6,
+        "name": "スマートファルコン",
+        "baba_aptitude": {"turf":"E","dirt":"A"},
+        "distance_aptitude":{"short":"B","mile":"A","middle":"A","long":"E"}
+    },
+    {
+        "id": 7,
+        "name": "マチカネフクキタル",
+        "baba_aptitude": {"turf":"A","dirt":"F"},
+        "distance_aptitude":{"short":"F","mile":"C","middle":"A","long":"A"}
+    },
+    {
+        "id": 8,
+        "name": "エルコンドルパサー",
+        "baba_aptitude": {"turf":"A","dirt":"B"},
+        "distance_aptitude":{"short":"F","mile":"A","middle":"A","long":"A"}
+    },
+    {
+        "id": 9,
+        "name": "ビワハヤヒデ",
+        "baba_aptitude": {"turf":"A","dirt":"F"},
+        "distance_aptitude":{"short":"F","mile":"C","middle":"A","long":"A"}
+    },
+    {
+        "id": 10,
+        "name": "ミホノブルボン",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"C","mile":"B","middle":"A","long":"B"}
+    },
+    {
+        "id": 11,
+        "name": "サクラバクシンオー",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"A","mile":"B","middle":"G","long":"G"}
+    },
+    {
+        "id": 12,
+        "name": "カレンチャン",
+        "baba_aptitude": {"turf":"A","dirt":"F"},
+        "distance_aptitude":{"short":"A","mile":"D","middle":"G","long":"G"}
+    },
+    {
+        "id": 13,
+        "name": "キングヘイロー",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"A","mile":"B","middle":"B","long":"C"}
+    },
+    {
+        "id": 14,
+        "name": "マルゼンスキー",
+        "baba_aptitude": {"turf":"A","dirt":"D"},
+        "distance_aptitude":{"short":"B","mile":"A","middle":"B","long":"C"}
+    },
+    {
+        "id": 15,
+        "name": "オグリキャップ",
+        "baba_aptitude": {"turf":"A","dirt":"B"},
+        "distance_aptitude":{"short":"E","mile":"A","middle":"A","long":"B"}
+    },
+    {
+        "id": 16,
+        "name": "ゴールドシップ",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"G","mile":"C","middle":"A","long":"A"}
+    },
+    {
+        "id": 17,
+        "name": "ウォッカ",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"F","mile":"A","middle":"A","long":"F"}
+    },
+    {
+        "id": 18,
+        "name": "ダイワスカーレット",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"F","mile":"A","middle":"A","long":"B"}
+    },
+    {
+        "id": 19,
+        "name": "グラスワンダー",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"G","mile":"A","middle":"B","long":"A"}
+    },
+    {
+        "id": 20,
+        "name": "テイエムオペラオー",
+        "baba_aptitude": {"turf":"A","dirt":"E"},
+        "distance_aptitude":{"short":"G","mile":"E","middle":"A","long":"A"}
+    },
+    {
+        "id": 21,
+        "name": "シンボリルドルフ",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"E","mile":"C","middle":"A","long":"A"}
+    },
+    {
+        "id": 22,
+        "name": "エアグルーヴ",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"C","mile":"B","middle":"A","long":"E"}
+    },
+    {
+        "id": 23,
+        "name": "マヤノトップガン",
+        "baba_aptitude": {"turf":"A","dirt":"E"},
+        "distance_aptitude":{"short":"D","mile":"D","middle":"A","long":"A"}
+    },
+    {
+        "id": 24,
+        "name": "メジロライアン",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"E","mile":"C","middle":"A","long":"B"}
+    },
+    {
+        "id": 25,
+        "name": "ライスシャワー",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"E","mile":"C","middle":"A","long":"A"}
+    },
+    {
+        "id": 26,
+        "name": "アグネスタキオン",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"G","mile":"D","middle":"A","long":"B"}
+    },
+    {
+        "id": 27,
+        "name": "ライスシャワー",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"E","mile":"C","middle":"A","long":"A"}
+    },
+    {
+        "id": 28,
+        "name": "ウイニングチケット",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"G","mile":"F","middle":"A","long":"B"}
+    },
+    {
+        "id": 29,
+        "name": "スーパークリーク",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"G","mile":"G","middle":"A","long":"A"}
+    },
+    {
+        "id": 30,
+        "name": "ナリタタイシン",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"F","mile":"D","middle":"A","long":"A"}
+    },
+    {
+        "id": 31,
+        "name": "ハルウララ",
+        "baba_aptitude": {"turf":"G","dirt":"A"},
+        "distance_aptitude":{"short":"A","mile":"B","middle":"G","long":"G"}
+    },
+    {
+        "id": 32,
+        "name": "ナイスネイチャ",
+        "baba_aptitude": {"turf":"A","dirt":"G"},
+        "distance_aptitude":{"short":"G","mile":"C","middle":"A","long":"A"}
+    },
+]
