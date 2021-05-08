3aabfe7da25ddd58cbc6cacdac24b30c1b9a44e0
citron888 <citron.socks.yuu10@gmail.com>
2021-05-08 21:34:36 +0900

    デザインの修正と新機能の枠組み実装

--- umamusu.js
+++ umamusu.js
@@ -1,10 +1,33 @@
 $(function(){
+  // datatable日本語化
+  $.extend( $.fn.dataTable.defaults, { 
+    language: {
+        url: "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json"
+    } 
+  }); 
+
+  // ウマ娘リスト作成
   umamusu_array =
     $.map(window.umamusu, function(value, index){
       return { id: value["id"], text: value["name"] }
     });
-  $(".select2").select2({
+  $(".umamusu-selecter .select2").select2({
     language: "ja",
     data: umamusu_array
   });
+  $('.umamusu-selecter .select2').val(null).trigger('change');
+  // datatable適用
+  $(".race_list").DataTable({
+    lengthChange: false,
+    paging: false
+  });
 });
+
+function show_race_list() {
+  parent_element = $("")
+  left_grandmother_element = $("")
+  right_grandmother_element = $("")
+  if(parent_element.val() != null){
+    console.log("test")
+  }
+}
---
+++ umamusu_compatibility.json.js
@@ -0,0 +1,8 @@
+window.umamusu_compatibility = [
+    {
+        "id": 1,
+        "parent_umamusu_id": 1,
+        "groudmother_umamusu_id": 2,
+        "value": 21,
+    },
+]
